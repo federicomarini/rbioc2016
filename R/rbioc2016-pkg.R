3312e75aaba083289736447d078d7beabdf5cd9d
Federico Marini <marinif@uni-mainz.de>
2016-06-02 11:40:22 +0200

    Initial commit

---
+++ R/rbioc2016-pkg.R
@@ -0,0 +1,11 @@
+#' rbioc2016 - datasets used in the course and other extra material
+#'
+#' rbioc2016 contains the following datasets
+#'
+#' @author
+#' Federico Marini \email{marinif@@uni-mainz.de}, 2016
+#'
+#' Maintainer: Federico Marini \email{marinif@@uni-mainz.de}
+#' @name rbioc2016
+#' @docType package
+NULL
---
+++ README.md
@@ -0,0 +1,7 @@
+# R/Bioconductor course 2016 companion package
+
+`rbioc2016` contains the datasets used in the course and other extra material
+
+## Author
+
+Federico Marini (marinif@uni-mainz.de)
