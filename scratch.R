63f6e0ca39ccc02de94a8cd7a53f2bd255e90be4
Federico Marini <marinif@uni-mainz.de>
2016-06-16 11:06:54 +0200

    Update scratch.R

--- scratch.R
+++ scratch.R
@@ -9,3 +9,20 @@ mygr <- GRanges(c(rep("chr1",5),rep("chr2",5)),
                 score = 1:10,
                 GC = seq(1, 0, length=10))
 
+
+
+
+mygenes <- genes(tdb)
+set.seed(42)
+affectedgenes_id <- sample(x = 1:20000, 300)
+affectedgenes <- mygenes[affectedgenes_id]
+# where are your promoters?!
+affectedpromoters <- promoters(affectedgenes)
+width(affectedpromoters)
+biocLite("BSgenome.Mmusculus.UCSC.mm10")
+library("BSgenome.Mmusculus.UCSC.mm10")
+Mmusculus
+affectedpromoterseqs <- getSeq(Mmusculus,affectedpromoters)
+affectedpromoterseqs
+writeXStringSet(affectedpromoterseqs,"affectedpromoterseqs.fasta",width = 50)
+
